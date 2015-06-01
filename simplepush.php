9d02a04be462915182b4f371bca656c5851909d0
chinmoy panda <chinmoy.panda@iglulabs.com>
2015-06-01 11:17:26 +0530

    script to use apn

---
+++ simplepush.php
@@ -0,0 +1,48 @@
+<?php
+
+// Put your device token here (without spaces):
+$deviceToken = 'c7859eab14d225db00d9cd82cafff5c692387b288f13947bf0cd444cfb90cfe2';
+// Put your private key's passphrase here:
+$passphrase = 'Dream True';
+
+// Put your alert message here:
+$message = 'Test push notification!';
+
+////////////////////////////////////////////////////////////////////////////////
+
+$ctx = stream_context_create();
+stream_context_set_option($ctx, 'ssl', 'local_cert', 'ck.pem');
+stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+// Open a connection to the APNS server
+$fp = stream_socket_client(
+	'ssl://gateway.sandbox.push.apple.com:2195', $err,
+	$errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+
+if (!$fp)
+	exit("Failed to connect: $err $errstr" . PHP_EOL);
+
+echo 'Connected to APNS' . PHP_EOL;
+
+// Create the payload body
+$body['aps'] = array(
+	'alert' => $message,
+	'sound' => 'default'
+	);
+
+// Encode the payload as JSON
+$payload = json_encode($body);
+
+// Build the binary notification
+$msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+
+// Send it to the server
+$result = fwrite($fp, $msg, strlen($msg));
+
+if (!$result)
+	echo 'Message not delivered' . PHP_EOL;
+else
+	echo 'Message successfully delivered' . PHP_EOL;
+
+// Close the connection to the server
+fclose($fp);
